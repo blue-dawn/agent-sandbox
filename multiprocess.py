7e65189106fb4a1513af3bc635aa2b18ee8814dc
yazoo1220 <podman1220@hotmail.com>
2023-07-26 01:05:32 +0300

    Update multiprocess.py

--- multiprocess.py
+++ multiprocess.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import time
 
 def worker(num):
@@ -6,5 +7,11 @@ def worker(num):
     time.sleep(0.5)
 
 if __name__ == '__main__':
+    jobs = []
     for i in range(5):
-        worker(i)
+        p = multiprocessing.Process(target=worker, args=(i,))
+        jobs.append(p)
+        p.start()
+
+    for job in jobs:
+        job.join()
